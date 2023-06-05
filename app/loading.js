4b38ff4f2f255789f05f227307527e09abc1da96
Bilal Hassan <bilalhassain14@gmail.com>
2023-06-05 23:59:55 +0500

    first commit

---
+++ app/loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="bg-blue-600 text-white p-20 text-center text-xl ">
+      Loading...
+    </div>
+  );
+};
+
+export default loading;
---
+++ app/not-found.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const error = () => {
+  return (
+    <div className="bg-red-600 text-white p-20 text-center text-xl ">
+      404 - Page not found
+    </div>
+  );
+};
+
+export default error;
